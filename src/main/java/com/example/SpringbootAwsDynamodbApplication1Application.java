42a2e50d224f7902be8dba17ef0123fda735bbee
anupam <anupam.nitdgp14@gmail.com>
2026-02-12 22:14:17 +0530

    Initialized the project.

---
+++ src/main/java/com/example/SpringbootAwsDynamodbApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDynamodbApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDynamodbApplication1Application.class, args);
+	}
+
+}
